2f8cdc2765689329be600456d00334ae2f808408
Shaikh Ahmad <167265931+ahmadcoder2248@users.noreply.github.com>
2024-05-25 06:33:19 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,66 @@
+let score = 'ahmad';
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);  //number me convert krne ke liye Number
+//console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+// '33' => 33
+// '33abc' => NaN
+// 'true' => 1; false => 0
+
+let isLoggedIn = 'ahmad'
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)   // conversion kiye boolean me convert kare
+//console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// '' => false
+// 'ahmad' => true
+
+let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber);
+
+
+// ************************************************** Operation *****************************************************************
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = 'hello';
+let str2 = ' Ahmad';
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log('1' + 2 );
+// console.log(1 + '2' );
+// console.log('1' + 2 + 2);  // string pahele haito pura string print karega
+// console.log(1 + 2 + '2');  // string last me haito pahele conversion ho jayega
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
