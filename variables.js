8534849cfdd3ed356601c5f6b75aa6e0381bff15
Shaikh Ahmad <167265931+ahmadcoder2248@users.noreply.github.com>
2024-05-25 06:32:24 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,22 @@
+const accountName = 'Ahmad';
+let accountEmail = 'shaikh@google.com';
+var accountPassword = '12345';
+accountCity = 'Mumbai';
+let accountState;
+
+//accountName = shaikh  // not allowed
+// once you assign value in const after that assign value you will not change the value in const variable
+// you can change value in let use only const and let  
+
+
+accountEmail = 'hc@hc.com';
+accountPassword = '21213455';
+accountCity = 'Delhi';
+console.log(accountName);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
