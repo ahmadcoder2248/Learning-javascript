3e3c014e76c924d0d388ac7be88f4213dfbb93c3
Shaikh Ahmad <167265931+ahmadcoder2248@users.noreply.github.com>
2024-05-25 06:32:47 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+'use strict'; // treat all js code as newer version
+
+console.log(3
+    +
+  3)  // code readability should be high 
+
+  let name = 'ahmad';
+  let age = 21;
+  let isLoogedIn = false;
+
+  /* primitive datatypes
+  number 
+  bigint
+  string => ''
+  boolean => true/false
+  null standalone
+  undifined => 
+  symbol => unique
+  */
+
+  // object
+  console.log(typeof undefined); // undifined
+  console.log(typeof null); // object
