569c359ed62905bed2f1e68afb5d8dfa387b4145
Shaikh Ahmad <167265931+ahmadcoder2248@users.noreply.github.com>
2024-05-25 06:35:37 +0530

    Add files via upload

---
+++ 07_String.js
@@ -0,0 +1,37 @@
+let firstName = 'ahmad'
+const lastName = 'shaikh'
+const age = '21'
+const whereYouStay = 'govandi'
+
+let valueName = firstName.length;
+
+
+
+const fullInformation = `MY NAME IS ${firstName.toUpperCase()} ${lastName.toUpperCase()} AND MY AGE IS ${age} I AM LEAVING IN ${whereYouStay.toUpperCase()}`
+console.log(fullInformation);
+
+const myAccountNumber = '7897'
+const finalAccount = myAccountNumber.padStart(16, '*')
+console.log(`MY ACCOUNT NUMBER IS ${finalAccount}`);
+
+/******* method (without argument)
+. toUpperCase()
+. toLowerCase()
+. trim()
+. trimStart()
+. trimEnd() */
+
+/* ******** method with argument
+. indexOf()
+. includes()
+. replace()
+. replaceAll()
+. concat()
+. padStart()
+. padEnd()
+. charAt()
+. charCodeAt()
+. split()
+
+*/ 
+
