151aa55208aa64e7b2e61fd045fd70db162f7fc8
Shaikh Ahmad <167265931+ahmadcoder2248@users.noreply.github.com>
2024-05-26 08:48:28 +0530

    Add files via upload

---
+++ 09_comparison_operators.js
@@ -0,0 +1,39 @@
+/* == equal to (equality) [it does implicit (aitomatic) conversion0]
+ === equal value and type (strict equality)
+ != not equal
+ !== not equal value or not equal type (strict)
+ > greater than
+ < less than
+ >= greater than equal to 
+ <= Less than equal to */
+
+  // (==)
+const userAge1 = 18
+const userAge2 = 24
+// console.log(userAge1==userAge2);
+
+// (===)
+const userAge3 = '24'
+const userAge4 = 24
+// console.log(user3===user4);
+
+// (!=)
+const userAge5 = 24
+const userAge6 = 24
+// console.log(userAge5 != userAge6);
+
+const user7 = '56'
+const user8 = 45
+//  console.log(user7 !== user8);
+
+// (>)
+const user9 = '21'
+const user10 = 24
+console.log(user10 > user9);
+
+
+
+
+
+
+
