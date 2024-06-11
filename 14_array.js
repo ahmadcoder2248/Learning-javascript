95d3e181a38305b083dd25445d13833432858f1c
Shaikh Ahmad <167265931+ahmadcoder2248@users.noreply.github.com>
2024-06-11 16:31:28 +0530

    Add files via upload

---
+++ 14_array.js
@@ -0,0 +1,43 @@
+/* 
+Basic Array Method
+1. pop()
+2. push()
+3. shift()
+4. unshift()
+5. concat()
+6. indexOf()
+7. includes()
+8. reverse()
+9. sort()
+10. slice()
+11. splice()
+*/
+
+
+const evenNumber = [0, 2 ,4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24]
+const animals = ['Dog', 'Cat', 'Rat', 'Lion', 'elephant', 'Cow', 'Tiger']
+const oddNumber = [1, 3, 5]
+
+
+
+
+// evenNumber.shift()
+// evenNumber.unshift(0)
+
+// const addedArray = evenNumber.concat(animals)
+// const addedArray2 = evenNumber.concat(evenNumber,oddNumber)
+
+// const elementIndex = animals.indexOf('Cat')
+// const isIncluded = animals.includes('Dog')
+
+// animals.sort()
+// animals.slice(5)
+// animals.slice(2, 5)
+
+const nameAndNumberList = [['Adarsh', 75], ['Akash', 90], ['Ahmad', 56], ['shaikh', 67,89,'rehan']]
+
+console.log(nameAndNumberList[0][1])
+
+
+
+
